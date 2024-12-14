df7fa1ce023c3042faece0351c53067ee150f0df
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-12-14 18:16:02 -0500

    feat: add mac and windows helix paths to update script

--- scripts/helix-update-config.sh
+++ scripts/helix-update-config.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
-# Only works on my mac rn
+# Assumes my folder structure for my devices
 
 echo "1. Copy from device to repo"
 echo "2. Update device from repo"
 read -p "Choose an action (1 or 2): " choice
 
+# Helix has a different config location for windows
+# Mac and Linux are the same
+isWindows=""
+isMacOrLinux=""
+
+if [[ "$OSTYPE" == "linux"* || "$OSTYPE" == "darwin"* ]]; then
+  isMacOrLinux="true"
+elif [[ "$OSTYPE" == "msys"* ]]; then
+  # git-bash
+  isWindows="true"
+fi
+
+
+copy_from_device() {
+  if [[ "$isWindows" ]]; then
+    echo "Copying from windows device to repo..."
+    rsync -rv --exclude '**/.git/' ~/AppData/Roaming/helix/ ~/source/config/helix/
+  elif [[ "$isMacOrLinux" ]]; then
+    echo "Copying from mac/linux device to repo..."
+    rsync -rv --exclude '**/.git/' ~/.config/helix/ ~/src/config/helix/
+  fi
+}
+
+copy_from_repo() {
+  if [[ "$isWindows" ]]; then
+    echo "Copying from repo to windows device..."
+    rsync -rv --exclude '**/.git/' ~/source/config/helix/ ~/AppData/Roaming/helix/ 
+  elif [[ "$isMacOrLinux" ]]; then
+    echo "Copying from repo to mac/linux device..."
+    rsync -rv --exclude '**/.git/' ~/src/config/helix/ ~/.config/helix/
+  fi
+}
+
+
 case "$choice" in 
   1)
-    echo "Copying from device to repo..."
-    rsync -rv --exclude '**/.git/' ~/.config/helix/ ~/src/config/helix
+    copy_from_device
     echo "Completed copy to repo"
     break
     ;;
   2)
     echo "Updating device from repo"
-    rsync -rv --exclude '**/.git/' ~/src/config/helix ~/.config/helix/
     echo "Completed copy from repo"
     break
     ;;
