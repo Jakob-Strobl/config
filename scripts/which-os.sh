50f6b670668695dd364c7e9c7e1c4c4eebea419c
Jakob-Strobl <jakobpstrobl@gmail.com>
2024-12-14 19:02:18 -0500

    feat: add windows git-bash to which os script

--- scripts/which-os.sh
+++ scripts/which-os.sh
@@ -4,6 +4,8 @@ if [[ "$OSTYPE" == "linux"* ]]; then
   echo "linux"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "macos"
+elif [[ "$OSTYPE" == "msys"* ]]; then
+  echo "windows"
 else
-  echo "unknown os"
+  echo "unknown os $OSTYPE"
 fi
